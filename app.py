bfa135d89c3705821b3b4eefd8d4b7378bbb790a
Axel Adrian Anaya Garcia <maddozs@pop-os.localdomain>
2020-02-06 12:09:01 -0600

    Works in some cases

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
+import json
+
+
 class Proceso():
+    entrada = 0
+    rafaga = 0
+    nombre = 0
+    wait = 0
+
     def __init__(self, rafaga, entrada, nombre):
         self.entrada = entrada
         self.rafaga = rafaga
@@ -8,84 +16,118 @@ class Proceso():
     def __str__(self):
         return f"'{self.nombre}'-'{self.rafaga}'-'{self.entrada}' "
 
+    def remaining_time(self, tiempo_del_cpu):
+        return self.rafaga - (tiempo_del_cpu - self.entrada - self.wait)
+
+
+def total_wait_time(*procesos):
+    total = 0
+    for proceso in procesos:
+        total += proceso.wait
+    return total
+
 
 def srtf(*procesos):
+    gantt = {}
     map_ordered_by_entry = {}
     total_time = 0
 
     for proceso in procesos:
-        
         total_time += proceso.rafaga
         if proceso.entrada in map_ordered_by_entry:
             map_ordered_by_entry[proceso.entrada].append(proceso)
         else:
             map_ordered_by_entry[proceso.entrada] = [proceso]
-        print(map_ordered_by_entry)
 
     wait_list = []
-    process = []
+    actual_process = None
 
+    print(map_ordered_by_entry)
     for i in range(0, total_time):
-        print(i)
-        print(process)
+        gantt[i] = {
+            "Proceso": None,
+            "Ráfaga restante": None,
+        }
+
         for proceso in wait_list:
-            proceso.wait = proceso.wait + 1
+            proceso.wait += 1
 
         if i in map_ordered_by_entry:
-            shortest = map_ordered_by_entry[i][0]
 
-            for proceso in map_ordered_by_entry[i][1:]:
-                print("lol")
-                if shortest.rafaga > proceso.rafaga:
+            # Si se ingresan nuevos procesos, se añaden a la lista de espera
+            aux_list = wait_list + map_ordered_by_entry[i]
+            # Siempre será el primero, se hace una comparación de cual de los
+            # procesos tiene menor tiempo de rafaga restante
+            shortest = aux_list[0]
+
+            # El tiempo de ráfaga restante se calcula con la siguiente formula
+            # tiempo restante = ráfaga - (momento del algoritmo - tiempo de entrada del proceso + tiempo de espera total del proceso)
+
+            for proceso in aux_list[1:]:
+                if shortest.remaining_time(i) > proceso.remaining_time(i):
                     shortest = proceso
                 else:
                     wait_list.append(proceso)
 
-            if i == 0:
-                process.append(shortest)
-            elif process[i-1].rafaga == 0:
-                process.append(shortest)
-            elif shortest.rafaga < process[i-1].rafaga:
-                process.append(shortest)
-                wait_list.append(process[i])
+            if i == 0 or actual_process.remaining_time(i) == 0:
+                actual_process = shortest
+
+                gantt[i]["Proceso"] = actual_process.nombre
+                gantt[i]["Ráfaga restante"] = actual_process.remaining_time(i)
+
+            elif shortest.remaining_time(i) < actual_process.remaining_time(i):
+                wait_list.append(actual_process)
+                actual_process = shortest
+
+                gantt[i]["Proceso"] = actual_process.nombre
+                gantt[i]["Ráfaga restante"] = actual_process.remaining_time(i)
+
             else:
                 wait_list.append(shortest)
-                process.append(process[i-1])
-                process[i].rafaga = process[i].rafaga - 1
-        else:
-            aux = process[i-1]
-            aux.rafaga = aux.rafaga - 1
-            process.append(aux)
 
-        if wait_list:
+                gantt[i]["Proceso"] = actual_process.nombre
+                gantt[i]["Ráfaga restante"] = actual_process.remaining_time(i)
+
+        elif actual_process.remaining_time(i) != 0:
+            gantt[i]["Proceso"] = actual_process.nombre
+            gantt[i]["Ráfaga restante"] = actual_process.remaining_time(i)
+
+        else:
             shortest = wait_list[0]
 
             for proceso in wait_list[1:]:
+                if shortest.remaining_time(i) >= proceso.remaining_time(i):
+                    if shortest.entrada > proceso.entrada:
+                        shortest = proceso
 
-                if shortest.rafaga > proceso.rafaga:
-                    shortest = proceso
-                elif shortest.rafaga == proceso.rafaga:
-                    shortest = proceso if proceso.entrada < shortest.entrada else shortest
+            gantt[i]["Proceso"] = shortest.nombre
+            gantt[i]["Ráfaga restante"] = shortest.remaining_time(i)
 
-            if process[i-1].rafaga == 0:
-                process.append(shortest)
+            wait_list.remove(shortest)
+            actual_process = shortest
 
-            if shortest.rafaga < process[i-1].rafaga:
-                process.append(shortest)
-                wait_list.append(process[i])
-
-            else:
-                process.append(process[i-1])
-                process[i].rafaga = process[i].rafaga - 1
-
-    return process
+    return gantt
 
 
-p1 = Proceso(4, 0, "p1")
-p2 = Proceso(3, 3, "p2")
-p3 = Proceso(1, 2, "p3")
+p1 = Proceso(7, 0, "p1")
+p2 = Proceso(4, 2, "p2")
+p3 = Proceso(1, 4, "p3")
+p4 = Proceso(4, 5, "P4")
 
-gantt = srtf(p1, p2, p3)
+procesos = [p1, p2, p3, p4]
+gantt = srtf(*procesos)
+total_waiting_time = total_wait_time(*procesos)
 
-for ob in gantt:
-    print(f"Proceso: '{ob.nombre}' ; rafaga: ''{ob.rafaga}''")
+# print("Resultado:\n", json.dumps(gantt, indent=1, sort_keys=True))
+i=0
+for instant in gantt:
+    process = gantt[instant]
+    print(f"{i}: {process}")
+    i+=1
+
+print("Tiempo de espera individuales: ")
+for proceso in procesos:
+    print(f"Proceso: {proceso.nombre} - Espero: {proceso.wait}")
+
+print("Total de tiempo en espera: ", total_waiting_time)
+print("Tiempo de espera promedio: ", total_waiting_time/len(procesos))
