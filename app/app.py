aece86450fa31160c6e197f105119f10e2e42ff4
Axel Anaya <axel_anaya@hotmail.com>
2020-02-07 11:13:46 -0600

    view update

--- app/app.py
+++ app/app.py
@@ -8,38 +8,48 @@ class Demo1:
         self.frame = tk.Frame(self.master)
         self.label = tk.Label(self.frame, text="Simulador de calendarización de procesos", justify='center')
         self.label.grid(row=0, column=0, sticky="we", columnspan=2, pady=40, padx = 20)
-
-        self.label2 = tk.Label(self.frame, text="Numero de procesos", justify='center')
-        self.label2.grid(row=1, column=0, sticky="we", columnspan=2, padx = 20)
-
-        self.input1 = tk.Entry(self.frame)
-        self.input1.grid(row=2, column=0, sticky="we", columnspan=2, padx = 20)
-
-        self.label3 = tk.Label(self.frame, text="Quantum (solo para RR)", justify='center')
-        self.label3.grid(row=3, column=0, sticky="we", columnspan=2, padx=20)
-
-        self.input2 = tk.Entry(self.frame)
-        self.input2.grid(row=4, column=0, sticky="we", columnspan=2, padx=20, pady = (0,40))
         
-        self.button1 = tk.Button(self.frame, text = 'SRTF', command = self.new_window, background='blue')
-        self.button1.grid(row=5, column=0)
+        self.button1 = tk.Button(self.frame, text = 'SRTF', command = self.add_process, background='#F3EBA3')
+        self.button1.grid(row=1, column=0, pady = (0,40))
 
-        self.button2 = tk.Button(self.frame, text = 'Round Robin', command = self.new_window, background='blue')
-        self.button2.grid(row=5, column=1)
+        self.button2 = tk.Button(self.frame, text = 'Round Robin', command = self.add_process, background='#F3EBA3')
+        self.button2.grid(row=1, column=1, pady = (0,40))
 
         self.frame.pack()
 
-    def new_window(self):
+    def add_process(self):
         self.newWindow = tk.Toplevel(self.master)
-        self.app = Demo2(self.newWindow)
+        self.app = AddProcessWindow(self.newWindow)
 
-class Demo2:
+class SRTFWindow:
     def __init__(self, master):
         self.master = master
         self.frame = tk.Frame(self.master)
-        self.quitButton = tk.Button(self.frame, text = 'Quit', width = 25, command = self.close_windows)
-        self.quitButton.pack()
+
+        self.label2 = tk.Label(self.frame, text="Ráfaga", justify='center')
+        self.label2.grid(row=0, column=0, sticky="we")
+
+        self.input2 = tk.Entry(self.frame)
+        self.input2.grid(row=0, column=1, sticky="we")
+
         self.frame.pack()
+
+    def close_windows(self):
+        self.master.destroy()
+
+class AddProcessWindow:
+    def __init__(self, master):
+        self.master = master
+        self.frame = tk.Frame(self.master)
+
+        self.label2 = tk.Label(self.frame, text="Ráfaga", justify='center')
+        self.label2.grid(row=0, column=0, sticky="we")
+
+        self.input2 = tk.Entry(self.frame)
+        self.input2.grid(row=0, column=1, sticky="we")
+
+        self.frame.pack()
+
     def close_windows(self):
         self.master.destroy()
 
