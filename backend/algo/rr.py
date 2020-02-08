e1e9f1fac0a1a53da3a833dd3e94261a5c350989
Rodrigo Hernández Góngora <rgongora@grupodel.mx>
2020-02-07 20:46:52 -0600

    added changes

--- backend/algo/rr.py
+++ backend/algo/rr.py
@@ -1,46 +1,44 @@
 class RR:
-    processes = []
-    total_time = 0
-    transcurred_time = 0
-    count_processes = 0
-    actual_process = None
-    quantum = 0
+processes = []
+total_time = 0
+transcurred_time = 0
+count_processes = 0
+actual_process = None
+quantum = 0
 
-    def __init__(self, quantum, *procesos):
-        self.processes = list(procesos)
-        self.quantum = quantum
+def __init__(self, quantum, *procesos):
+    self.processes = list(procesos)
+    self.quantum = quantum
 
-        for proceso in procesos:
-            self.total_time += proceso.rafaga
-            self.count_processes += 1
+    for proceso in procesos:
+        self.total_time += proceso.rafaga
+        self.count_processes += 1
 
 
 
-    def rr(self):
-        gantt = {}
-        while self.transcurred_time != self.total_time:
+def rr(self):
+    gantt = {}
+    while self.transcurred_time != self.total_time:
 
-            for proceso in self.processes:
-                print(proceso.nombre, " ", proceso.rafaga)
-                proceso.wait = self.transcurred_time - proceso.summon
-                if proceso.rafaga > self.quantum:
-                    proceso.rafaga -= self.quantum
-                    self.transcurred_time += self.quantum
-                    proceso.summon = self.transcurred_time
-                else:
-                    self.transcurred_time += proceso.rafaga
-                    proceso.summon = self.transcurred_time
-                    proceso.rafaga -= proceso.rafaga
+        for proceso in self.processes:
+            gantt[self.transcurred_time] = {}
+            gantt[self.transcurred_time]["Proceso"] = proceso.nombre
+            gantt[self.transcurred_time]["Ráfaga restante"] = proceso.rafaga
+            print(proceso.nombre, " ", proceso.rafaga)
+            proceso.wait = self.transcurred_time - proceso.summon
+            if proceso.rafaga > self.quantum:
+                proceso.rafaga -= self.quantum
+                self.transcurred_time += self.quantum
+                proceso.summon = self.transcurred_time
+            else:
+                self.transcurred_time += proceso.rafaga
+                proceso.summon = self.transcurred_time
+                proceso.rafaga = 0
 
-                gantt[self.transcurred_time] = {}
-                gantt[self.transcurred_time]["Proceso"] = proceso.nombre
-                gantt[self.transcurred_time]["Ráfaga restante"] = proceso.rafaga
+        for proceso in self.processes:
+            if proceso.rafaga == 0:
+                self.processes.remove(proceso)
 
-            for proceso in self.processes:
-                if proceso.rafaga == 0:
-                    self.processes.remove(proceso)
-
-        return gantt
-
+    return gantt
 
 
